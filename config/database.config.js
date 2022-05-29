3d26e9d0d4c52eab314f57d195ee005a4fb9a30a
UmmLuqman <211771@astanait.edu.kz>
2022-05-29 17:58:38 +0600

    First commit

---
+++ config/database.config.js
@@ -0,0 +1,3 @@
+odule.exports = {
+    // url: 'mongodb://localhost:27017/newDatabase'
+    url:'mongodb+srv://greeyyyjupiter:Aitu2022!@cluster0.vyrk1.mongodb.net/myDatabase?retryWrites=true&w=majority',
